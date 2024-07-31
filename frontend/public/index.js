3e031d457bd3aee3962387843b60077b6eb92f1a
ymind14563 <ymind14563@gmail.com>
2024-07-31 15:40:20 +0900

    test: 두번째 커밋

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,17 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const resultBox = document.querySelector(`.result`);
+    const resultBox = document.getElementById(`todo-list`);
 
     function getTodos() {
         fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(res => res.json())
+        .then(res => res.json().slice(0, 10))
         .then(json => {
             for (i = 0; i <= 10; i++){
 
             resultBox.innerHTML = 
             `<li>
-            <input type="checkbox" id=${i}>${json[i].title}</input>
+            <input type="checkbox">${json[i].title}</input>
             <button onclick="deleteTodo()">X</button>
             </li>
             `
@@ -19,22 +19,63 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
     }
+
+
+    const addTodo = () => {
+        if (input.value !== '') {
+            const item = document.createElement('div')
+            
+            // 체크박스
+            const checkbox = document.createElement('input')
+            checkbox.type='checkbox'
+            
+            // text
+            const text = document.createElement('li');
+            
+            // 제거하기 버튼
+            const deleteButton = document.createElement('button')
+            deleteButton.textContent="X"
+
+            text.textContent = input.value
+            input.value=''
+        
+            item.appendChild(checkbox)
+            item.appendChild(text)
+            item.appendChild(deleteButton)
+            todoList.appendChild(item)
+
+
+            // 체크박스 이벤트 리스너
+            checkbox.addEventListener('change', (event) =>{ 
+                if (event.currentTarget.checked)
+                {
+                    text.style.textDecoration='line-through'
+                    text.style.color='gray'
+                }
+                else {
+                    text.style.textDecoration='none'
+                }
+            })
+
+            // 제거하기 버튼 클릭 이벤트 리스너
+            deleteButton.addEventListener('click', (event) => {
+                todoList.removeChild(event.currentTarget.parentNode)
+            })
+            input.value =''
+            alert.textContent = ''
+        }
+        else
+            alert.textContent = '할 일을 입력하세요!'
+    }
+
+    addButton.addEventListener('click', addTodo)
+
+    // 할 일 입력창에서 enter key가 눌렸을 때
+    input.addEventListener('keypress', (event) => {
+        const ENTER = 13
+        if (event.keyCode === ENTER)
+            addTodo();
+    })
+
+    getTodos();
 })
-
-let inputBox = document.getElementById('inputField');
-let addTodo = document.getElementById('addToDo');
-
-
-addTodo.addEventListener('click', function() {
-let list = document.createElement('li');
-if (!inputBox.value || inputBox.value === ' '){
-    alert('내용을 입력해주세요.')
-} else {
-
-    
-}
-})
-
-function deleteTodo(){
-    document.querySelector()
-}
